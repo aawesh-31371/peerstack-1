118031f9636ac65da109b0122bb21d3be0cfa5d6
aawesh <aawesh31371@gmail.com>
2026-04-23 15:44:06 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,28 @@
+// const mongoose = require("mongoose");
+
+// async function connectDB() {
+//   const mongoUri = process.env.MONGODB_URI;
+
+//   if (!mongoUri) {
+//     throw new Error("MONGODB_URI is missing. Add it to your .env file.");
+//   }
+
+//   await mongoose.connect(mongoUri);
+//   console.log("MongoDB connected");
+// }
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
-async function connectDB() {
-  const mongoUri = process.env.MONGODB_URI;
-
-  if (!mongoUri) {
-    throw new Error("MONGODB_URI is missing. Add it to your .env file.");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
   }
-
-  await mongoose.connect(mongoUri);
-  console.log("MongoDB connected");
-}
+};
 
 module.exports = connectDB;

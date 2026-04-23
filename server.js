83013044378a428a60ff1712af9ade9317c67ce4
aawesh <aawesh31371@gmail.com>
2026-04-23 16:22:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 
 const path = require("path");
 const express = require("express");
@@ -12,6 +13,7 @@ const projectRoutes = require("./routes/projects");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(__dirname));
